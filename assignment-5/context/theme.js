3149c8f5fe7628b6a07004547ed4f5314f594b9d
kev-69 <bismarkobuobi19@gmail.com>
2024-06-26 20:39:25 +0000

    Added context file for toggling theme

---
+++ assignment-5/context/theme.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+const themeContext = createContext();
+
+const themeProvider = ({children}) => {
+    const [darkTheme, setDarkTheme] = useState(false);
+
+    const toggleTheme = () => {
+        setDarkTheme(!darkTheme);
+    };
+
+    return (
+        <themeContext.Provider value={{darkTheme, toggleTheme}}>
+            {children}
+        </themeContext.Provider>
+    )
+};
+
+export {themeProvider, themeContext};
