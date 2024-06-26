2b0493aca29cb03a512d8d500a25a88bb39fda49
kev-69 <bismarkobuobi19@gmail.com>
2024-06-26 23:19:06 +0000

    Finalizing app.js

--- assignment-5/App.js
+++ assignment-5/App.js
@@ -1,17 +1,57 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useContext } from 'react';
+import { themeContext, themeProvider } from './context/theme';
+import HomeScreen from "./screens/HomeScreen"
+import SettingsScreen from './screens/SettingsScreen';
+import { Ionicons } from "@expo/vector-icons";
 
 
 const Tab = createBottomTabNavigator();
 
-function App() {
+function MyTabs() {
+  const { darkTheme } = useContext(themeContext);
+
+  return(
+    <Tab.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+
+        if (route.name === "Home") {
+          iconName = focused ? "home" : "home-outline";
+        } else if (route.name === "Transaction") {
+          iconName = focused ? "card" : "card-outline";
+        } else if (route.name === "Settings") {
+          iconName = focused ? "settings" : "settings-outline";
+        } else if (route.name === "Statistics") {
+          iconName = focused ? "stats-chart" : "stats-chart-outline";
+        }
+
+        return <Ionicons name = {iconName} size = {size} color = {color} />
+      },
+
+      tabBarActiveTintColor: "blue",
+      tabBarInactiveTintColor: "gray",
+      tabBarStyle: {
+        backgroundColor: darkTheme ? "#333" : "#fff"
+      },
+      headerShown: false,
+    })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name='Settings' component={SettingsScreen} />
+    </Tab.Navigator>
+  )
+}
+
+const App =() => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <themeProvider>
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
+    </themeProvider>
   );
 }
 
