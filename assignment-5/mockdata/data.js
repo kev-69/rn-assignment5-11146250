1efb2102fc421d3ead4f7ed1a8f7816044b88437
kev-69 <bismarkobuobi19@gmail.com>
2024-06-26 20:40:37 +0000

    Mock data

---
+++ assignment-5/mockdata/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        id: 0,
+        name: "Apple Store",
+        type: "Entertainment",
+        price: 5.99,
+        image: require("../assets/apple.png")
+    },
+    {
+        id: 1,
+        name: "Spotify",
+        type: "Music",
+        price: 12.99,
+        image: require("../assets/spotify.png"),
+    },
+    {
+        id: 2,
+        name: "Money Transfer",
+        type: "Transaction",
+        price: 300,
+        image: require("../assets/moneyTransfer.png"),
+    },
+    {
+        id: 3,
+        name: "Banana",
+        type: "Grocery",
+        price: 88,
+        image: require(),
+    }
+]
+
+export default data;
